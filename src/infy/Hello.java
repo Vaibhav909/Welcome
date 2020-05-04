562bef8e4749ce3c41f0863fd221850b728de10c
unknown <vaibhavpatil66@yahoo.com>
2020-05-04 19:16:39 +0530

    Deploy

--- src/infy/Hello.java
+++ src/infy/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2nd day n build");
+		System.out.println("2nd day  build");
 
 	}
 
